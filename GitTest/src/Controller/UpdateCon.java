737d9484e1c2e77c366246c4ea928fb8be765c31
inhwan0506@naver.com <inhwan0506@naver.com>
2021-12-30 12:45:11 +0900

    a

--- GitTest/src/Controller/UpdateCon.java
+++ GitTest/src/Controller/UpdateCon.java
@@ -6,19 +6,38 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class UpdateCon
- */
+import DAO.Member_DAO;
+import DTO.Member_DTO;
+
 @WebServlet("/UpdateCon")
 public class UpdateCon extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+		HttpSession session = request.getSession();
+		request.setCharacterEncoding("utf-8");
+		String mem_id = request.getParameter("mem_id");
+		String mem_name = request.getParameter("mem_name");
+		String mem_nick = request.getParameter("mem_nick");
+		String mem_phone = request.getParameter("mem_phone");
+		String mem_pw = request.getParameter("mem_pw");
+		
+		Member_DTO dto1 = new Member_DTO(mem_id,mem_name,mem_nick,mem_phone, mem_pw);
+		Member_DAO dao = new Member_DAO();
+				
+		Member_DTO dto = dao.Update(mem_id, mem_pw, mem_name,mem_nick,mem_phone);
+		System.out.println(dto.getMem_id());
+		if(dto.getMem_id()!=null) {
+			session.setAttribute("dto", dto);
+			response.sendRedirect("main.jsp");
+		}else {
+			
+		}
+	
+	
+	
 	}
 
 }
